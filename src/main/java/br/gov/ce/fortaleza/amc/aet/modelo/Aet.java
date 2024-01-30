8097ffd56c47dfdcc39c95c0ee4635e0f0e347fe
Joao Bosco de Vasconcelos Junior <bosco.junior@amc.fortaleza.ce.gov.br>
2024-01-30 11:40:16 -0300

    start of creating Aet entity

---
+++ src/main/java/br/gov/ce/fortaleza/amc/aet/modelo/Aet.java
@@ -0,0 +1,30 @@
+// package br.gov.ce.fortaleza.amc.aet.modelo;
+// import java.time.LocalDateTime;
+// import org.springframework.data.annotation.CreatedDate;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.Temporal;
+// import jakarta.persistence.TemporalType;
+// import lombok.Getter;
+// import lombok.Setter;
+
+// @Entity
+// @Table(name = "Aet")
+// @Getter
+// @Setter
+// public class Aet {
+
+//     @Column(name = "Aet_Num", nullable = false)
+//     private int aetNum;
+//     @Column(name = "Prc_Cod")
+//     private int prcCod;
+//     @Column(name = "Aet_Ano", nullable = false)
+//     private int aetAno;
+//     @Column(name = "Aet_DtHrRegistro", nullable = false)
+//     @Temporal(TemporalType.TIMESTAMP)
+//     @CreatedDate
+//     private LocalDateTime aetDtHrRegistro;
+
+    
+// }
